97ebfa6ec208f2d8a11c87343accdb57cfb1a1c7
Anthony Torres <anthony_torres@me.com>
2019-11-20 17:02:22 -0600

    added login & register, installed semantic

---
+++ src/CreateListingForm/index.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+class CreateListing extends Component {
+	constructor(){
+		super();
+
+		this.state= {
+			client_name: '',
+			client_number: '',
+			property_address: '',
+			list_price: ''
+		}
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			[e.currentTarget.name]: e.currentTarget.value
+		})
+	}
+
+	render(){
+		return(
+			<div>
+				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+				  Create Listing
+				</button>
+				<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				  <div class="modal-dialog" role="document">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <h5 class="modal-title" id="exampleModalLabel">New Listing</h5>
+				        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				          <span aria-hidden="true">&times;</span>
+				        </button>
+				      </div>
+				      <div class="modal-body">
+				          <form onSubmit={(e) => this.props.addListing(e, this.state)}>
+					          <label>Client name:</label>
+					          <form.input type='text' name='client_name' value={this.state.client_name} onChange={this.handleChange}/>
+					          <label>Client number:</label>
+					          <form.input type='text' name='client_number' value={this.state.client_number} onChange={this.handleChange}/>
+					          <label>Property address:</label>
+					          <form.input type='text' name='property_address' value={this.state.property_address} onChange={this.handleChange}/>
+					            <label>List price:</label>
+					          <form.input type='text' name='list_price' value={this.state.list_price} onChange={this.handleChange}/>
+					          <button type='Submit'>Create Dog</button>
+					        </form>
+				      </div>
+				      <div class="modal-footer">
+				        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				        <button type="button" class="btn btn-primary">Save changes</button>
+				      </div>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			)
+	}
+}
---
+++ src/ListingsList/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function ListingsList(props) {
+	const listings = props.listings.map(listing => {
+		return (
+			<div class='card' key={listing.id}>
+				<div class='card-body'>
+					<div class='card-title'>{listing.property_address}</div>
+					<p class='card-text'>
+					{listing.client_name}
+					{listing.client_number}
+					{listing.list_price}
+					</p>
+				</div>
+					<button type='button' class='btn btn-primary' onClick={() => props.deleteListing(listing.id)}>Delete Listing</button>
+					<button type='button' class='btn btn-primary' onClick={() => props.editListing(listing.id)}>Edit Listing</button>
+			</div>
+		);
+	});
+	
+ return (
+      // <Card.Group>
+        { listings }
+      // </Card.Group>
+    )
+}
+
+export default DogList
